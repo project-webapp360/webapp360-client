43a592b5ad0f4f74fb2c1a38577299b66ce21f8d
project-webapp360 <webapp360@yandex.ru>
2022-02-24 17:05:40 +0300

    allwillbeperfect

--- src/components/survey/Survey.js
+++ src/components/survey/Survey.js
@@ -1,5 +1,5 @@
 import React, {useState} from 'react';
-import {Checkbox, FormControlLabel, FormGroup} from "@mui/material";
+import {FormControl, FormControlLabel, FormLabel, Radio, RadioGroup} from Yrial";
 import "./Survey.css"
 import Modal from "../modal/Modal";
 
@@ -17,146 +17,20 @@ const Survey = ({visible, setVisible}) => {
     "Иванов предлагает новые идеи и решения?",
     "Иван умеет находить общий язык со своими коллегами?",
   ])
-  const [answers, setAnswers] = useState([0,0,0,0,0,0,0,0,0,0])
+  const [answers, setAnswers] = useState([0,0,0,0,0,0,0,0,0,0,])
   const [page, setPage] = useState(1)
-
-  const [array1, setArray1] = useState(false)
-  const [array2, setArray2] = useState(false)
-  const [array3, setArray3] = useState(false)
-  const [array4, setArray4] = useState(false)
-
-  const [disArray1, setDisArray1] = useState(false)
-  const [disArray2, setDisArray2] = useState(false)
-  const [disArray3, setDisArray3] = useState(false)
-  const [disArray4, setDisArray4] = useState(false)
+  const [value, setValue] = useState(0);
 
   const decrease = (event) => {
+    console.log(page)
     setPage(page-1)
-    handleCheckBox0()
-    if (answers[page - 1] === 1) {
-      handleCheckBox1(event)
-      console.log(1)
-    }
-    if (answers[page - 1] === 2) {
-      handleCheckBox2(event)
-      console.log(2)
-    }
-    if (answers[page - 1] === 3) {
-      handleCheckBox3(event)
-      console.log(3)
-    }
-    if (answers[page - 1] === 4) {
-      handleCheckBox4(event)
-      console.log(4)
-    }
+    getAnswer(page-2)
   }
 
   const increase = (event) => {
+    console.log(page)
     setPage(page+1)
-    handleCheckBox0()
-    if (answers[page - 1] === 1) {
-      setArray1(true)
-      setDisArray2(true)
-      setDisArray3(true)
-      setDisArray4(true)
-      console.log(1)
-    }
-    if (answers[page - 1] === 2) {
-      handleCheckBox2(event)
-      console.log(2)
-    }
-    if (answers[page - 1] === 3) {
-      handleCheckBox3(event)
-      console.log(3)
-    }
-    if (answers[page - 1] === 4) {
-      handleCheckBox4(event)
-      console.log(4)
-    }
-  }
-
-  const handleCheckBox0 =() => {
-    setArray1(false)
-    setArray2(false)
-    setArray3(false)
-    setArray4(false)
-    setDisArray1(false)
-    setDisArray2(false)
-    setDisArray3(false)
-    setDisArray4(false)
-  }
-
-  const handleCheckBox1 =(event) => {
-    if(array1 === false) {
-      setArray1(true)
-      setDisArray2(true)
-      setDisArray3(true)
-      setDisArray4(true)
-      const ans = 1
-      setAnswers(answers[page-1]=ans)
-      console.log(answers);
-    } else {
-      setArray1(false)
-      setDisArray2(false)
-      setDisArray3(false)
-      setDisArray4(false)
-      const ans = 0
-      setAnswers(answers[page-1]=ans)
-    }
-  }
-  const handleCheckBox2 =(event) => {
-    if(array2 === false) {
-      setArray2(true)
-      setDisArray1(true)
-      setDisArray3(true)
-      setDisArray4(true)
-      const ans = 2
-      setAnswers(answers[page-1]=ans)
-      console.log(answers);
-    } else {
-      setArray2(false)
-      setDisArray1(false)
-      setDisArray3(false)
-      setDisArray4(false)
-      const ans = 0
-      setAnswers(answers[page-1]=ans)
-    }
-  }
-  const handleCheckBox3 =(event) => {
-    if(array3 === false) {
-      setArray3(true)
-      setDisArray1(true)
-      setDisArray2(true)
-      setDisArray4(true)
-      const ans = 3
-      setAnswers(answers[page-1]=ans)
-      console.log(answers);
-    } else {
-      setArray3(false)
-      setDisArray1(false)
-      setDisArray2(false)
-      setDisArray4(false)
-      const ans = 0
-      setAnswers(answers[page-1]=ans)
-    }
-  }
-  const handleCheckBox4 =(event) => {
-    if(array4 === false) {
-      setArray4(true)
-      setDisArray1(true)
-      setDisArray2(true)
-      setDisArray3(true)
-      const ans = 4
-      setAnswers(answers[page-1]=ans)
-      console.log(answers);
-    } else {
-      setArray4(false)
-      setDisArray1(false)
-      setDisArray2(false)
-      setDisArray3(false)
-      const ans = 0
-      setAnswers(answers[page-1]=ans)
-    }
+    getAnswer(page)
   }
 
   const [modal,setModal] = useState(false);
@@ -173,6 +47,16 @@ const Survey = ({visible, setVisible}) => {
     setVisible(false)
   }
 
+  const handleRadioChange = (event) => {
+    setValue(event.target.value)
+    answers[page-1] = event.target.value
+  };
+
+  const getAnswer = (num) => {
+    const timed = answers[num]
+    setValue(timed)
+  };
+
   return (
     <div>
       <Modal visible={modal} setVisible={setModal}>
@@ -186,13 +70,21 @@ const Survey = ({visible, setVisible}) => {
       </Modal>
       <div className="survey__main">
         <div className="survey__first-line-number">Вопрос {page}</div>
-        <div className="survey__first-line">{questions[page-1]}</div>
-        <FormGroup>
-          <FormControlLabel onChange={handleCheckBox1} className="survey__test-form" control={<Checkbox/>} checked={array1} disabled={disArray1} label="Редко" />
-          <FormControlLabel onChange={handleCheckBox2} className="survey__test-form" control={<Checkbox/>} checked={array2} disabled={disArray2} label="Часто" />
-          <FormControlLabel onChange={handleCheckBox3} className="survey__test-form" control={<Checkbox/>} checked={array3} disabled={disArray3} label="Всегда" />
-          <FormControlLabel onChange={handleCheckBox4} className="survey__test-form" control={<Checkbox/>} checked={array4} disabled={disArray4} label="Никогда" />
-        </FormGroup>
+        <FormControl variant="standard">
+        <FormLabel className="survey__first-line">{questions[page-1]}</FormLabel>
+        <RadioGroup
+          className="survey__test-form"
+          defaultValue={value}
+          name="radio-buttons-group"
+          value={value}
+          onChange={handleRadioChange}
+        >
+          <FormControlLabel value="1" control={<Radio />} label="Редко" />
+          <FormControlLabel value="2" control={<Radio />} label="Часто" />
+          <FormControlLabel value="3" control={<Radio />} label="Всегда" />
+          <FormControlLabel value="4" control={<Radio />} label="Никогда" />
+        </RadioGroup>
+      </FormControl>
         <div className="survey__navigation">
           <div className="survey__page">{page} из 10</div>
           {page > 1
