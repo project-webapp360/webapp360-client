534a8f0be2e009822b9cc1928a7a282739519ce6
project-webapp360 <webapp360@yandex.ru>
2022-03-02 21:31:48 +0300

    i hope this works (i too)

--- src/pages/mainpage/mainpage.js
+++ src/pages/mainpage/mainpage.js
@@ -1,10 +1,7 @@
-import React, {useContext, useEffect, useState} from 'react';
+import React, {useContext, useEffect} from 'react';
 import Progressbar from "../../components/progressbar/progressbar";
 import Eventcard from "../../components/eventcard/Eventcard";
 import "./mainpage.css"
-import NavBar from "../../components/с_navBar/NavBar";
-import Modal from "../../components/modal/Modal";
-import AddEvent from "../../components/addEvent/AddEvent";
 import {Context} from "../../index";
 import {getEvents} from "../../axios/API";
 import {observer} from "mobx-react-lite";
@@ -12,8 +9,6 @@ import {observer} from "mobx-react-lite";
 const Mainpage = observer(() => {
 
   const {events} = useContext(Context)
-
-
 
   useEffect(   () => {
     async function fetchData() {
@@ -24,76 +19,9 @@ const Mainpage = observer(() => {
   })
 
 
-  //
-  // console.log(JSON.parse(localStorage.getItem("events")))
-  // console.log(events.events)
-
-  // useEffect(async () => {
-  //   events.setEvents(await getEvents())
-  // })
-
-  const [EventCardData, setEventCardData] = useState([
-    {
-    title: "Название",
-    dateStart:"2022-02-23",
-    dateEnd:"Конец",
-    name:'Вася',
-    creator:'Менеджер'
-    },
-    {
-      title: "Название",
-      dateStart:"2022-02-23",
-      dateEnd:"Конец",
-      name:'Вася',
-      creator:'Менеджер'
-    },
-    {
-      title: "Название",
-      dateStart:"2022-02-23",
-      dateEnd:"Конец",
-      name:'Вася',
-      creator:'Менеджер'
-    },
-    {
-      title: "Название",
-      dateStart:"2022-02-23",
-      dateEnd:"Конец",
-      name:'Вася',
-      creator:'Админ'
-    },
-    {
-      title: "Название",
-      dateStart:"2022-02-23",
-      dateEnd:"Конец",
-      name:'Вася',
-      creator:'Менеджер'
-    },])
-  //
-  //
-  //
-  // const changeVisible1 = () => {
-  //   setModal1(true)
-  // }
-  //
-  // const changeVisible2 = () => {
-  //   setModal2(true)
-  // }
-  //
-  // const changeVisible3 = () => {
-  //   setModal3(true)
-  // }
-  //
-  // const createEvent = (newEvent) => {
-  //   setEventCardData([...EventCardData, newEvent] )
-  // }
-
-
   return (
     <div>
-
-      {/*<NavBar setVisible1={changeVisible1} setVisible2={changeVisible2} setVisible3={changeVisible3}/>*/}
       <Progressbar/>
-      <h1>Victor is not okey</h1>
       {
         events.events.map((item) => <Eventcard title={item.title} dateStart={item.dateStart} dateEnd={item.dateEnd} name={item.name} creator={item.creator}/>)
       }
