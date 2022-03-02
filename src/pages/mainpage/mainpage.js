2cd68ca0a70d491e14f4a78bc28cc208a4d21ee9
project-webapp360 <webapp360@yandex.ru>
2022-03-02 20:47:34 +0300

    i hope this works (i too)

--- src/pages/mainpage/mainpage.js
+++ src/pages/mainpage/mainpage.js
@@ -87,9 +87,10 @@ const Mainpage = observer(() => {
 
       {/*<NavBar setVisible1={changeVisible1} setVisible2={changeVisible2} setVisible3={changeVisible3}/>*/}
       <Progressbar/>
-      {
-        events.events.map((item) => <Eventcard title={item.title} dateStart={item.dateStart} dateEnd={item.dateEnd} name={item.name} creator={item.creator}/>)
-      }
+      <h1>Victor is not okey</h1>
+      {/*{*/}
+      {/*  events.events.map((item) => <Eventcard title={item.title} dateStart={item.dateStart} dateEnd={item.dateEnd} name={item.name} creator={item.creator}/>)*/}
+      {/*}*/}
     </div>
   );
 });
